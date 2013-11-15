7f4aeff8d0319925230fcb47757d73f4d820b006
VirtualUbuntu <gianluca@ubuntu.(none)>
2013-11-15 12:18:05 +0100

    Aggiornato codice client

--- src/client/card/ClientLoader.java
+++ src/client/card/ClientLoader.java
@@ -1,7 +1,7 @@
 package card;
 
-import java.io.Console;
 import java.rmi.*;
+import java.io.*;
 
 public class ClientLoader{
 	
@@ -14,13 +14,23 @@ public class ClientLoader{
 		String host = args[0];
 		
 		try{
-			LoaderAuthInt proxy = Naming.lookup("//" + host + "/proxy");
-			Runnable client = proxy.login(console.read("Username> "), console.readPassword("Password"));
+			String[] lista = Naming.list("//" + host + "/authServer");
+			for( int i = 0; i < lista.length; i = i+1)
+				System.out.println(lista[i]);
+				
+			LoaderAuthInt authServer = (LoaderAuthInt)Naming.lookup("//" + host + "/authServer");
+			System.out.println("Lookup eseguita.");
+			if( authServer != null)
+				System.out.println("authServer != null.");
+				
+			Runnable client = authServer.login(console.readLine("Username: "), new String(console.readPassword("Password: ")));
+			
 			if(client == null)
 				System.err.println("Login non valido");
 			else
 				client.run();
 			
+			System.out.println("ADDIO");
 		}catch(Exception e){
 			System.err.println(e);
 			System.exit(1);
